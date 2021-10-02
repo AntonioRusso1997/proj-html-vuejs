5f4b93e63f2846b1f44992dadf0c6366421b23d1
Antonio <antonioru1997@gmail.com>
2021-10-02 12:35:05 +0200

    Speakers finito con hover

--- src/components/main-components/Speakers.vue
+++ src/components/main-components/Speakers.vue
@@ -11,7 +11,14 @@
         </div>
         <div class="row w-100 py-4">
           <div v-for="(speaker, index) in speakers" :key="index" class="speaker-card p-3 col-3 d-flex flex-column align-items-center justify-content-center">
-            <img class="w-100 my-2" :src="require('../../assets/' + speaker.path)" alt="">
+            <div class="img-wrap w-100 p-0 my-2">
+              <img class="w-100 m-0" :src="require('../../assets/' + speaker.path)" alt="">
+              <div class="hover-card h-100 w-100 d-flex justify-content-center align-items-center">
+                <i class="fab fa-twitter"></i>
+                <i class="fab fa-facebook-f"></i>
+                <i class="fab fa-linkedin-in"></i>
+              </div>
+            </div>
             <div class="speaker-name mt-3">{{speaker.name.toUpperCase()}}</div>
             <div class="speaker-job">{{speaker.job}}</div>
 
@@ -62,16 +69,46 @@ export default {
       color: white;
     }
   }
-  .speaker-name {
-    font-size:15px;
-    font-weight: 600;
-    letter-spacing: 2px;
-  }
-  .speaker-job {
-    font-size: 14px;
-    font-weight: 500;
-    font-style: italic;
-    color: $gray;
+  .speaker-card {
+
+    .speaker-name {
+      font-size:15px;
+      font-weight: 800;
+      letter-spacing: 2px;
+    }
+    .speaker-job {
+      font-size: 14px;
+      font-weight: 500;
+      font-style: italic;
+      color: $gray;
+    }
+    .img-wrap {
+        position: relative;
+  
+      img {
+        border-radius: 5px;
+      }
+      .hover-card {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 22px;
+        color: $white;
+        background-color: rgba($color: #e20000, $alpha: 0.5);
+        border-radius: 5px;
+        z-index: 999;
+        display: none !important;
+
+        i {
+          margin: 10px;
+          cursor: pointer;
+        }
+      }
+    }
+    &:hover .hover-card {
+      display: flex !important;
+    }
   }
 }
 
